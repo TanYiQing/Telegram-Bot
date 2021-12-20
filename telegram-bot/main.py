94580ceb0746ea646cab71cb0b961edb43082686
TanYiQing <yiqingtan99@gmail.com>
2021-12-20 23:16:32 +0800

    update

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -21,15 +21,33 @@ def start_command(update, context):
 
 
 def main_menu(update, context):
-    update.callback_query.message.edit_text("Please choose a function to proceed.", reply_markup=main_menu_keyboard())
+    update.callback_query.message.edit_text("Please choose a function to proceed.",
+                                            reply_markup=main_menu_keyboard())
 
 
 def help_menu(update, context):
-    update.callback_query.message.edit_text("What can I help you? Please do not hesitate to contact us.", reply_markup=help_menu_keyboard())
+    update.callback_query.message.edit_text("What can I help you? Please do not hesitate to contact us.",
+                                            reply_markup=help_menu_keyboard())
 
 
 def contact_menu(update, context):
-    update.callback_query.message.edit_text("Contact Number: 0103373164\nEmail: yiqingtan99@gmail.com", reply_markup=contact_menu_keyboard())
+    update.callback_query.message.edit_text("Contact Number: 0103373164\nEmail: yiqingtan99@gmail.com",
+                                            reply_markup=contact_menu_keyboard())
+
+
+def about_menu(update, context):
+    update.callback_query.message.edit_text("Find out more about me!",
+                                            reply_markup=about_menu_keyboard())
+
+
+def about_me_menu(update, context):
+    update.callback_query.message.edit_text("https://pkob-270607.herokuapp.com/",
+                                            reply_markup=aboutme_menu_keyboard())
+
+
+def web_menu(update, context):
+    update.callback_query.message.edit_text("https://pkob-270607.herokuapp.com/",
+                                            reply_markup=website_menu_keyboard())
 
 
 def handle_message(update, context):
@@ -71,7 +89,7 @@ def main_menu_keyboard():
     keyboard = [
         [InlineKeyboardButton('Get Info', callback_data='getdata')],
         [InlineKeyboardButton('Help', callback_data='help'),
-         InlineKeyboardButton('About This Bot', callback_data='main')]
+         InlineKeyboardButton('About This Bot', callback_data='about')]
     ]
     return InlineKeyboardMarkup(keyboard)
 
@@ -82,13 +100,32 @@ def getinfo_keyboard():
 
 
 def help_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('Contact Us', callback_data='contact')],
-                [InlineKeyboardButton('Back', callback_data='main')]]
+    keyboard = [[InlineKeyboardButton('Contact Us', callback_data='contactus')],
+                [InlineKeyboardButton('Back to main menu', callback_data='main')]]
     return InlineKeyboardMarkup(keyboard)
 
 
 def contact_menu_keyboard():
-    keyboard = [InlineKeyboardButton('Back', callback_data='help')]
+    keyboard = [[InlineKeyboardButton('Back to previous', callback_data='help')],
+                [InlineKeyboardButton('Back to main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def about_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('About Us', callback_data='aboutus'), InlineKeyboardButton('Our Website', callback_data='web')],
+                [InlineKeyboardButton('Back to main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def aboutme_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Back to previous', callback_data='about')],
+                [InlineKeyboardButton('Back to main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def website_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Back to previous', callback_data='about')],
+                [InlineKeyboardButton('Back to main menu', callback_data='main')]]
     return InlineKeyboardMarkup(keyboard)
 
 
@@ -100,7 +137,10 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('start', start_command))
     dp.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
     dp.add_handler(CallbackQueryHandler(help_menu, pattern='help'))
-    dp.add_handler(CallbackQueryHandler(contact_menu, pattern='contact'))
+    dp.add_handler(CallbackQueryHandler(contact_menu, pattern='contactus'))
+    dp.add_handler(CallbackQueryHandler(about_menu, pattern='about'))
+    dp.add_handler(CallbackQueryHandler(about_me_menu, pattern='aboutus'))
+    dp.add_handler(CallbackQueryHandler(web_menu, pattern='web'))
     dp.add_handler(CommandHandler('getdata', get_data_command))
 
     # Messages
